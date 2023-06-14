47b4f0ad85c74b7a23e750701cf65bd6038bc4ae
Ranganath MK <94040412+ranganathmk27@users.noreply.github.com>
2023-06-14 16:01:05 +0530

    Create TestMethods.java

---
+++ TestMethods.java
@@ -0,0 +1,54 @@
+import org.testng.annotations.Test;
+
+public class TestMethods {
+    
+    @Test(description = "verify successful login")
+    public void testLogin() {
+        // Test implementation
+    }
+    
+    @Test
+    public void testLogout() {
+        // Test implementation
+    }
+    
+    @Test(description = "verify user registration")
+    public void testRegisterUser() {
+        // Test implementation
+    }
+    
+    @Test
+    public void testResetPassword() {
+        // Test implementation
+    }
+    
+    @Test(description = "verify account activation process")
+    public void testActivateAccount() {
+        // Test implementation
+    }
+    
+    @Test
+    public void testSearch() {
+        // Test implementation
+    }
+    
+    @Test(description = "verify add to cart functionality")
+    public void testAddToCart() {
+        // Test implementation
+    }
+    
+    @Test(description = "verify checkout process")
+    public void testCheckout() {
+        // Test implementation
+    }
+    
+    @Test
+    public void testCancelOrder() {
+        // Test implementation
+    }
+    
+    @Test(description = "verify order tracking")
+    public void testTrackOrder() {
+        // Test implementation
+    }
+}
